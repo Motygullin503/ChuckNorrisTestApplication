dccbb948a4d66a75ac595cb46d4f9e9a803c5e0a
Bulat Motygullin <bul3515@gmail.com>
2019-09-26 09:03:35 +0300

    Navigation

--- app/src/main/java/ru/test/chucknorristestapplication/view/ui/joke/JokeFragment.kt
+++ app/src/main/java/ru/test/chucknorristestapplication/view/ui/joke/JokeFragment.kt
@@ -1,6 +1,43 @@
 package ru.test.chucknorristestapplication.view.ui.joke
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import ru.test.chucknorristestapplication.R
+import ru.test.chucknorristestapplication.viewmodel.JokeFragmentViewModel
 
-class JokeFragment: Fragment() {
+class JokeFragment : Fragment() {
+
+    private val mViewModel: JokeFragmentViewModel by lazy {
+        ViewModelProviders.of(this).get(JokeFragmentViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_joke, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        configureViews()
+        observeViewModel()
+    }
+
+    private fun configureViews() {
+
+
+    }
+
+    private fun observeViewModel() {
+
+
+    }
+
 }
--- app/src/main/java/ru/test/chucknorristestapplication/view/ui/main/MainActivity.kt
+++ app/src/main/java/ru/test/chucknorristestapplication/view/ui/main/MainActivity.kt
@@ -1,13 +1,51 @@
 package ru.test.chucknorristestapplication.view.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import ru.test.chucknorristestapplication.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBar(navController, appBarConfiguration)
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfig: AppBarConfiguration
+    ) {
+
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment))
+                || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
